38247111e7c45c74b1813719fb0bbdedcb4d920f
Emir Özbir <emirozbir@kloia.com>
2020-03-22 04:35:32 +0300

    db connection added

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
-import "fmt"
-
-/*
 import (
 	"fmt"
-)*/
-
-
-
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
 func main()  {
 
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	quickstartDatabase := client.Database("manas")
+	podcastsCollection := quickstartDatabase.Collection("processes")
 
 	containers := ContainerList()
 	procList   := SearchFullProc()
 	j := 0
+
+
 
 	for range containers {
 		containerPid := getContainerPid(containers[j])
@@ -23,15 +39,41 @@ func main()  {
 		obj := generateCompareObject(objectProcess)
 		fmt.Print(obj)
 
+		containerProcessList, err := podcastsCollection.InsertOne(ctx, bson.D{
+			{"pid", obj.pid},
+			{"ipc", obj.ipc},
+			{"cmd", obj.cmd},
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Print(containerProcessList.InsertedID)
+
 		j++
 	}
 
 	k := 0
 
 	for range procList {
+
 		objectProcess := Process{procList[k]}
 		mainPid := generateCompareObject(objectProcess)
 		fmt.Print(mainPid)
+
+		mainPidList, err := podcastsCollection.InsertOne(ctx, bson.D{
+			{"pid", mainPid.pid},
+			{"ipc", mainPid.ipc},
+			{"cmd", mainPid.cmd},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Print(mainPidList.InsertedID)
 		k++
 	}
+
+
 }
