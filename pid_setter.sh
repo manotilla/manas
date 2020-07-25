9ca73cb0885b1679b522c347e0b3865ef7d33780
WoodProgrammer <emirozbir@kloia.com>
2020-07-26 02:24:09 +0300

    IPC connector added

--- pid_setter.sh
+++ pid_setter.sh
@@ -2,17 +2,22 @@
 
 ALL_PIDS=$(ps -ef|awk '{print $2}')
 
-for pid in $ALL_PIDS; do
-	CMD_LINE=$(cat "/proc/$pid/cmdline" 2>/dev/null)
+while true
+do
+	for pid in $ALL_PIDS; do
+		echo "PID: $pid"
+		CMD_LINE=$(cat "/proc/$pid/cmdline"|tr -d '\0')
 
-	IPC=$(ls -la "/proc/$pid/ns" 2>err.log|awk '{split($0,a,"->"); print a[2]}'|grep "ipc")
+		IPC=$(ls -la "/proc/$pid/ns" 2>err.log|awk '{split($0,a,"->"); print a[2]}'|grep "ipc")
 
-	if [[ "$IPC" == *"ipc:["* ]]
-	then
-		echo "$IPC"
-		echo "$CMD_LINE"
-		#redis-cli -n 0 HMSET $IPC $pid $CMD_LINE
-	else
-		echo "empty IPC output"
-	fi
+		if [[ "$IPC" == *"ipc:["* ]]
+		then
+			echo "$IPC"
+			echo "$CMD_LINE"
+			redis-cli -n 0 HMSET $pid $IPC "process-$CMD_LINE"
+		else
+			echo "empty IPC output"
+		fi
+	done
+
 done
