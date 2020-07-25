64e00a6ed13f3811db688146909cc0e290c87104
WoodProgrammer <emirozbir@kloia.com>
2020-07-25 20:28:50 +0300

    shell implemnetation added

--- pid_setter.sh
+++ pid_setter.sh
@@ -1,13 +1,18 @@
-
 #!/bin/bash
 
 ALL_PIDS=$(ps -ef|awk '{print $2}')
 
 for pid in $ALL_PIDS; do
 	CMD_LINE=$(cat "/proc/$pid/cmdline" 2>/dev/null)
-	IPC=$(ls -la '/proc/$pid/ns' 2>/dev/null|awk '{split($0,a,"->"); print a[2]}'|grep "ipc")
-	redis-cli -n 0 HMSET $IPC $pid $CMD_LINE
 
+	IPC=$(ls -la "/proc/$pid/ns" 2>err.log|awk '{split($0,a,"->"); print a[2]}'|grep "ipc")
+
+	if [[ "$IPC" == *"ipc:["* ]]
+	then
+		echo "$IPC"
+		echo "$CMD_LINE"
+		#redis-cli -n 0 HMSET $IPC $pid $CMD_LINE
+	else
+		echo "empty IPC output"
+	fi
 done
-
-
