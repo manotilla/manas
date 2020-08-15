166f136046e072cc2f460b3094381ff4fd19ab4a
WoodProgrammer <emirozbir@kloia.com>
2020-08-15 16:50:41 +0300

    updated to object based calling type

--- api/proc_info.py
+++ api/proc_info.py
@@ -1,33 +1,35 @@
 import redis
 import os
 
-REDIS_HOST=os.environ["REDIS_HOST"]
+class Proc(object):
 
-redis_conn = redis.Redis(host=REDIS_HOST, port=6379, db=0, charset="utf-8", decode_responses=True)
+    def __init__(self):
+        REDIS_HOST=os.environ["REDIS_HOST"]
+        self.redis_conn = redis.Redis(host=REDIS_HOST, port=6379, db=0, charset="utf-8", decode_responses=True)
 
-def getContainerIPC(container_id):
-    search_str="container-"+container_id
+    def getContainerIPC(self, container_id):
+        search_str="container-"+container_id
 
-    data = redis_conn.hget(search_str, "ipc")
+        data = self.redis_conn.hget(search_str, "ipc")
 
-    return data
+        return data
 
-def getIPCProcesses(ipc):
+    def getIPCProcesses(self, ipc):
 
-    data = redis_conn.lrange(ipc, 0, 5)
+        data = self.redis_conn.lrange(ipc, 0, 10)
 
-    return data
+        return data
 
 
-def generateProcResponse():
+    def generateProcResponse(self, ):
 
-    containers = ["29e4110398ac", "51c396f0d899"]
-    response = []
+        containers = ["c2efb9b3a283", "ec6a3a1f7e1b"]
+        response = []
 
-    for container in containers:
-        ipc = getContainerIPC(container)
-        tmpJson = {"ipc": ipc, "processes":getIPCProcesses(ipc)}
-        response.append(tmpJson)
+        for container in containers:
+            ipc = self.getContainerIPC(container)
+            tmpJson = {"container":container, "ipc": ipc, "processes":self.getIPCProcesses(ipc)}
+            response.append(tmpJson)
 
-    return response
+        return response
 
--- api/service.py
+++ api/service.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 @app.route("/dashboard", methods=['GET'])
 def dashboard():
-    response = generateProcResponse()
+    obj = Proc()
+    response = obj.generateProcResponse()
     return render_template("dashboard.html", processes=response)
+    #jsonify(response)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
